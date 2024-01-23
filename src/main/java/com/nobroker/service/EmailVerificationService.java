f13569e89684ac832b9ec4b9b787fe6a6846db04
shaur <sk11sk095r@gmail.com>
2024-01-23 14:22:56 +0530

    program flow of EmailVerificationService

--- src/main/java/com/nobroker/service/EmailVerificationService.java
+++ src/main/java/com/nobroker/service/EmailVerificationService.java
@@ -35,3 +35,17 @@ public class EmailVerificationService {
         return response;
     }
 }
+
+
+
+//1> user registers  --->> provides (name, mobile,email, pasword)[some more fields are there that are associated with the user table in the  db and that is the --Id field is auto generated  and emailVerify is set to false [this boolean field will be true once the email  through otp verification is done ]]
+//
+//2> email is then sent to the provided email and meanwhile  the user is regsitered(saved in the db)
+//
+//3> when the otp is sent to the email provided a hashmap stores the  email and otp in the key value pair
+//
+//4 when we  devlop  the verify Otp  module   user enters its email and the otp user  has got via  email  ,
+//       4.1 now we  compare the otp  that was sent to the email{ that is already stored in the has map at the time of sending otp to the mail } and the otp  that is enterd by the user
+//       4.2 after verification of otp  it is checked whether the user exists with the particular email id {taken at the starting of otp verification}
+//       4.3 now that  the  verification is done  the  field  emailVerify is set to true earlier which was false
+//       4.4 and if  verification is not sucessfull then it will  return  the  message of not verified accordingly.
